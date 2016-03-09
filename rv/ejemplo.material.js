1ebf956842384574034f81d2c3af3ddb52f7920b
Luis4000 <jlgntroy@hotmail.com>
2016-03-09 10:52:38 -0600

    Update ejemplo.material.js

--- rv/ejemplo.material.js
+++ rv/ejemplo.material.js
@@ -12,3 +12,15 @@ renderer =new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
 }
+//Loop
+function loop(){
+requestAnimationFrame(loop);
+malla.rotation.x +=0.01;
+malla.rotation.y +=0.01;
+renderer.render(escena,camara);
+}
+
+//Se declaran simbolos globales y se ejecuta setup y loop
+var escena,camara,renderer, malla;
+setup();
+loop();
